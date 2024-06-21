0aeeaca7151f90c4d432cbb3171d9a255acc730a
chaeyeon-yang <myday0827@naver.com>
2024-06-21 16:17:24 +0900

    chore: 커스텀 컬러

---
+++ src/styles/styled.d.ts
@@ -0,0 +1,15 @@
+import 'styled-components';
+
+declare module 'styled-components' {
+  export interface DefaultTheme {
+    mainColor: string;
+    // fontLarge: number;
+    // fontMedium: number;
+    // fontRegular: number;
+    // fontSmall: number;
+
+    // weightBold: number;
+    // weightMedium: number;
+    // weightRegular: number;
+  }
+}
