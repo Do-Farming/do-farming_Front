fcbb640136bbb24f7e9162248eee9924219a2bbe
JeongHeum Byun <quswjdgma83@gmail.com>
2024-06-27 17:04:00 +0900

    feat: 라우팅설정

--- App.tsx
+++ App.tsx
@@ -26,8 +26,11 @@ import CompetitiveScreen from './src/screens/compation/CompetitiveScreen';
 import ProductSignUpScreen from './src/screens/product/ProductSignUpScreen';
 import ProductPasswordScreen from './src/screens/product/auth/ProductPasswordScreen';
 import ProductSignInScreen from './src/screens/product/complete/ProductSignInScreen';
+<<<<<<< HEAD
 import MyPageScreen from './src/screens/mypage/MyPageScreen';
 import ProductSignUpScreen from './src/screens/product/ProductSignUpScreen';
+=======
+>>>>>>> 4592e23 (feat: 라우팅설정)
 
 const Stack = createNativeStackNavigator();
 const Tab = createBottomTabNavigator();
@@ -150,11 +153,14 @@ export default function App() {
             component={ProductSignInScreen}
             options={{ title: '가입완료' }}
           />
+<<<<<<< HEAD
           <Stack.Screen
             name="ProductSignUp"
             component={ProductSignUpScreen}
             options={{ title: '입출금' }}
           />
+=======
+>>>>>>> 4592e23 (feat: 라우팅설정)
         </Stack.Navigator>
       </NavigationContainer>
     </StyledComponentsThemeProvider>
