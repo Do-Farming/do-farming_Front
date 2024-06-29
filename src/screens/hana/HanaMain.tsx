afb9598dddd287e80d10a7a297471e6b372f8477
Minjoo-kang123 <69236634+Minjoo-kang123@users.noreply.github.com>
2024-06-29 15:04:57 +0000

    수정 완료

--- src/screens/hana/HanaMain.tsx
+++ src/screens/hana/HanaMain.tsx
@@ -9,7 +9,6 @@ import {
   NativeSyntheticEvent,
   NativeScrollEvent,
 } from "react-native";
-import Carousel from 'react-native-sna  p-carousel';
 import {
   Container,
   Header,
@@ -37,191 +36,17 @@ import {
   Product3
 } from "./HomeScreen.styled";
 import axios from "axios";
-
+import {mainContents, saleContents} from "../../mocks/hanaMainDatas";
 const { width: windowWidth } = Dimensions.get("window");
 
-const products2 = [
-  {
-    images: [
-      require("../../assets/hanamain1.png"),
-      require("../../assets/arrow.png")
-    ],
-    descriptrions:{
-      text1:"로그인하고 안전하게",
-      text2:"잔액을 조회하세요"
-    },
-    interestRate: {
-      max: "가져오기",
-      min: "1.0%",
-    },
-    btnText: {
-      option1: "가져오기",
-      option2: "보내기",
-    }
-  },
-  {
-    images: [
-      require("../../assets/hanamain2.png"),
-      require("../../assets/arrow.png")
-    ],
-    descriptrions:{
-      text1:"마이데이터로 스마트하게",
-      text2:"자산을 관리하세요"
-    },
-    interestRate: {
-      max: "가져오기",
-      min: "1.0%",
-    },
-    btnText: {
-      option1: "자산 등록",
-      option2: "보내기",
-    }
-  },
-  {
-    images: [
-      require("../../assets/hanamain3.png"),
-      require("../../assets/arrow.png")
-    ],
-    descriptrions:{
-      text1:"로그인하고 안전하게",
-      text2:"잔액을 조회하세요"
-    },
-    interestRate: {
-      max: "가져오기",
-      min: "1.0%",
-    },
-    btnText: {
-      option1: "가져오기",
-      option2: "보내기",
-    }
-  },
-  {
-    images: [
-      require("../../assets/hanamain4.png"),
-      require("../../assets/arrow.png")
-    ],
-    descriptrions:{
-      text1:"로그인하고 안전하게",
-      text2:"잔액을 조회하세요"
-    },
-    interestRate: {
-      max: "가져오기",
-      min: "1.0%",
-    },
-    btnText: {
-      option1: "가져오기",
-      option2: "보내기",
-    }
-  },
-];
 
-const products = [
-  {
-    images: [
-      require("../../assets/hanafolder/moneydream.png")
-    ],
-    descriptrions:{
-      text:"여름! 돈기운 상점 오픈",
-      text1:"진짜 금을 준다고?",
-      text2:"황금 베개 응모 꾹!"
-    },
-  },
-  {
-    images: [
-      require("../../assets/hanafolder/security.png")
-    ],
-    descriptrions:{
-      text:"무서운 보이스피싱",
-      text1:"하나가 지켜드려요!",
-      text2:"#사이버금융범죄"
-    },
-  },
-  {
-    images: [
-      require("../../assets/hanafolder/walletSoccer.png")
-    ],
-    descriptrions:{
-      text:"우승 적금 출시 기념",
-      text1:"최애 축구팀 응원하고",
-      text2:"금리와 경품을 동시에"
-    },
-  },
-  {
-    images: [
-      require("../../assets/hanafolder/girl.png")
-    ],
-    descriptrions:{
-      text:"소중한 내 월급!",
-      text1:"매달 더해지는 달달함까지",
-      text2:"#100만 하나머니"
-    },
-  },{
-    images: [
-      require("../../assets/hanafolder/kid.png")
-    ],
-    descriptrions:{
-      text:"우리아이 용돈관리 공식",
-      text1:"아이부자 출시 3주년",
-      text2:"매일매일 경품 추첨!"
-    },
-  },
-  {
-    images: [
-      require("../../assets/hanafolder/money.png")
-    ],
-    descriptrions:{
-      text:"비상금 대출 이벤트",
-      text1:"비상금 충전하고",
-      text2:"하나머니 혜택까지"
-    },
-  },
-  {
-    images: [
-      require("../../assets/hanafolder/ballon.png")
-    ],
-    descriptrions:{
-      text:"혜택 알림 신청",
-      text1:"알아서 쓱 날아오는",
-      text2:"풍성한 혜택"
-    },
-  },
-  {
-    images: [
-      require("../../assets/hanafolder/twosome.png")
-    ],
-    descriptrions:{
-      text:"7월 1일은 하나데이~",
-      text1:"애플망고 빙수+커피 2잔",
-      text2:"랜덤 하나머니 100% 당첨"
-    },
-  },
-  {
-    images: [
-      require("../../assets/hanafolder/soccer.png")
-    ],
-    descriptrions:{
-      text:"하나원큐 축구Play",
-      text1:"매일매일 미션 참여하고",
-      text2:"매일매일 원큐볼 받자!"
-    },
-  },
-  {
-    images: [
-      require("../../assets/hanafolder/travel.png")
-    ],
-    descriptrions:{
-      text:"해외송금 이벤트!",
-      text1:"지정만 해도 헤택이",
-      text2:"#3만 하나머니"
-    },
-  },
-];
+
 
 const Pagination: React.FC<{ length: number; currentIndex: number }> = ({
   currentIndex,
 }) => (
   <PaginationContainer>
-    {Array.from({ length: products.length }, (_, index) => (
+    {Array.from({ length: saleContents.length }, (_, index) => (
       <PaginationDot key={index} isActive={index === currentIndex} />
     ))}
   </PaginationContainer>
@@ -246,7 +71,7 @@ export default function HanaMain() {
   useEffect(() => {
     const interval = setInterval(() => {
       setCurrentIndex((prevIndex) =>
-        prevIndex === products.length - 1 ? 0 : prevIndex + 1
+        prevIndex === saleContents.length - 1 ? 0 : prevIndex + 1
       );
     }, 3000);
 
@@ -274,11 +99,11 @@ export default function HanaMain() {
     setCurrentIndex(index);
   };
   const handleNext = () => {
-    setCurrentIndex((prevIndex) => (prevIndex + 1) % products.length);
+    setCurrentIndex((prevIndex) => (prevIndex + 1) % saleContents.length);
   };
 
   const handlePrev = () => {
-    setCurrentIndex((prevIndex) => (prevIndex - 1 + products.length) % products.length);
+    setCurrentIndex((prevIndex) => (prevIndex - 1 + saleContents.length) % saleContents.length);
   };
   const handleMomentumScrollEnd2 = (
     event: NativeSyntheticEvent<NativeScrollEvent>
@@ -297,7 +122,7 @@ export default function HanaMain() {
       <MainProduct>
         <FlatList
           ref={flatListRef2}
-          data={products2}
+          data={mainContents}
           horizontal
           pagingEnabled
           showsHorizontalScrollIndicator={false}
@@ -332,12 +157,12 @@ export default function HanaMain() {
           )}
           onMomentumScrollEnd={handleMomentumScrollEnd2}
         />
-        <Pagination2 length={products2.length} currentIndex2={currentIndex2} />
+        <Pagination2 length={mainContents.length} currentIndex2={currentIndex2} />
       </MainProduct>
       <MainProduct>
         <FlatList
           ref={flatListRef}
-          data={products}
+          data={saleContents}
           horizontal
           pagingEnabled
           showsHorizontalScrollIndicator={false}
@@ -353,7 +178,7 @@ export default function HanaMain() {
                     <HanaPaginationButton onPress={handlePrev}>
                       <Text>{'<'}</Text>
                     </HanaPaginationButton>
-                    <HanaPageNumber>{`${currentIndex + 1} / ${products.length}`}</HanaPageNumber>
+                    <HanaPageNumber>{`${currentIndex + 1} / ${saleContents.length}`}</HanaPageNumber>
                     <HanaPaginationButton onPress={handleNext}>
                       <Text>{'>'}</Text>
                     </HanaPaginationButton>
