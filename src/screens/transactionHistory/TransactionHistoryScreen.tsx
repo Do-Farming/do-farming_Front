4f9a9338174a3b9773b71ae50ce00d6b8bd97a51
LimEunSang <dmstkd2905@naver.com>
2024-07-01 10:46:52 +0900

    chore: copy code 삭제

--- src/screens/transactionHistory/TransactionHistoryScreen.tsx
+++ src/screens/transactionHistory/TransactionHistoryScreen.tsx
@@ -1,57 +1,6 @@
 import React, { useState } from 'react';
-import {
-  Container,
-  Header,
-  Profile,
-  ProfileImage,
-  ProfileName,
-  Menu,
-  MenuItem,
-  MenuItemImage,
-  MenuItemText,
-  MenuItemButton,
-} from './MyPageScreen.styled';
-import ChallengingTabView from './challengingTabView/ChallengingTabView';
-import MyAccountView from './myAccountView/MyAccountView';
+import { Container } from './TransactionHistoryScreen.styled';
 
 export default function MyPageScreen({ navigation }: any) {
-  const [selectedView, setSelectedView] = useState('challenging');
-
-  return (
-    <Container>
-      <Header>
-        <Profile>
-          <ProfileImage source={require('../../assets/1.png')} />
-          <ProfileName>홍길동님</ProfileName>
-        </Profile>
-        <Menu>
-          <MenuItem>
-            <MenuItemButton
-              onPress={() => {
-                setSelectedView('challenging');
-              }}
-            >
-              <MenuItemImage source={require('../../assets/challenging.png')} />
-            </MenuItemButton>
-            <MenuItemText>챌린징</MenuItemText>
-          </MenuItem>
-          <MenuItem>
-            <MenuItemButton
-              onPress={() => {
-                setSelectedView('myAccount');
-              }}
-            >
-              <MenuItemImage source={require('../../assets/myaccount.png')} />
-            </MenuItemButton>
-            <MenuItemText>내 계좌</MenuItemText>
-          </MenuItem>
-        </Menu>
-      </Header>
-      {selectedView === 'challenging' ? (
-        <ChallengingTabView />
-      ) : (
-        <MyAccountView />
-      )}
-    </Container>
-  );
+  return <Container></Container>;
 }
