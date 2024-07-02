141ba079883ed983c4af19a1cc0334ee9033ee5c
LimEunSang <dmstkd2905@naver.com>
2024-07-02 16:54:55 +0900

    fix: 존재하지 않는 파일 참조로 인한 오류

--- src/assets/svg/index.ts
+++ src/assets/svg/index.ts
@@ -1,4 +1,3 @@
 export { default as RunIcon } from './ic_run.svg';
 export { default as DoFarmingIcon } from './ic_doFarming.svg';
 export { default as GreenCheckIcon } from './ic_greenCheck.svg';
-export { default as HanaLogo } from './ic_hanalogo.svg';
