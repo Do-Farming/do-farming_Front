9aba01b8337a58ff75c91ad68c1905a83af4fc57
chaeyeon-yang <myday0827@naver.com>
2024-07-03 15:45:17 +0900

    feat: 객체 탐지 일치성 확인 후 처리

--- src/screens/wakeup/camera/CameraScreen.styled.ts
+++ src/screens/wakeup/camera/CameraScreen.styled.ts
@@ -7,7 +7,6 @@ export const Container = styled.View`
 
 export const PreviewContainer = styled.View`
   flex: 1;
-  justify-content: center;
   align-items: center;
 `;
 
@@ -29,4 +28,23 @@ export const ButtonContainer = styled.View`
 export const Button = styled.Pressable`
   flex: 1;
   align-items: center;
+`;
+
+export const DetectResult = styled.Text`
+  font-size: 17px;
+  font-weight: 700;
+  color: green;
+  margin-top: 20px;
+`;
+
+export const DetectFail = styled.Text`
+  font-size: 17px;
+  font-weight: 700;
+  color: #e22f2f;
+  margin-top: 20px;
+  margin-bottom: 20px;
+`;
+
+export const DetectView = styled.View`
+  flex-direction: column;
 `;
