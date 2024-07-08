8c41986e0c1ee1938d797ea2535c818ce57331f8
chaeyeon-yang <myday0827@naver.com>
2024-07-08 20:31:53 +0900

    feat: 전체 상품 조회 페이지 api 통신

---
+++ src/constants/Product.ts
@@ -0,0 +1,8 @@
+import { ObjectMappingType } from './WakeupObject';
+
+export const ObjectMapping: ObjectMappingType = {
+  SAVING: '예금',
+  CHECKING: '입출금',
+  DOFARMING: '두파밍',
+  DEPOSIT: '적금',
+};
---
+++ src/types/product/ProductTypes.ts
@@ -0,0 +1,7 @@
+export interface ProductType {
+  prodCode: string;
+  prodName: string;
+  joinPeriod: string;
+  prodRate: string;
+  type: string;
+}
