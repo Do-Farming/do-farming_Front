a4da2293c1850410421669c03d2a6cad6da262a9
chaeyeon-yang <myday0827@naver.com>
2024-07-09 21:27:21 +0900

    docs: 브랜치 푸시

--- src/screens/mypage/MyPageScreen.tsx
+++ src/screens/mypage/MyPageScreen.tsx
@@ -26,6 +26,19 @@ export default function MyPageScreen({ navigation }: any) {
   const [myInfo, setMyInfo] = useState<MyInfo>();
   const [myAccountList, setMyAccountList] = useState<Account[]>([]);
   const [myGroup, setMyGroup] = useState<Group>();
+  const [loading, setLoading] = useState<boolean>(true);
+  const [userImg, setUserImg] = useState<number>(0);
+
+  interface profileType {
+    [key: number]: string;
+  }
+
+  const profileImages: profileType = {
+    1: require('../../assets/1.png'),
+    2: require('../../assets/2.png'),
+    3: require('../../assets/3.png'),
+    4: require('../../assets/4.png'),
+  };
 
   useEffect(() => {
     const getMyInfo = async () => {
@@ -34,6 +47,7 @@ export default function MyPageScreen({ navigation }: any) {
           await axiosInstance.get<ApiResponse<MyInfo>>(`/api/v1/customer/me`);
         if (response.data.isSuccess) {
           setMyInfo(response.data.result);
+          setUserImg(response.data.result.userImg);
         } else {
           console.log('내 정보 데이터 요청 실패');
         }
@@ -72,21 +86,31 @@ export default function MyPageScreen({ navigation }: any) {
     };
 
     if (isLogin) {
-      getMyInfo();
-      getMyAccountList();
-      getMyGroup();
+      Promise.all([getMyInfo(), getMyAccountList(), getMyGroup()]).then(() =>
+        setLoading(false),
+      );
     } else {
       navigation.navigate('SignIn');
     }
   }, [isLogin]);
 
+  if (loading) {
+    return <div>로딩 중...</div>;
+  }
+  console.log(myInfo?.userImg);
+
   return (
     <Container>
       <Header>
-        <Profile>
-          <ProfileImage source={require('../../assets/1.png')} />
-          <ProfileName>{myInfo?.name}님</ProfileName>
-        </Profile>
+        {myInfo && (
+          <Profile>
+            <ProfileImage
+              source={require(profileImageList[myInfo?.userImg - 1])}
+            />
+            <ProfileName>{myInfo?.name}님</ProfileName>
+          </Profile>
+        )}
+
         <Menu>
           <MenuItem>
             <MenuItemButton
